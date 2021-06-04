41c97e51b4572b07a3e07f79c2cf8a0cf5e43f82
Sumit <sumitdesai1997@gmail.com>
2021-06-04 13:05:14 +0530

    playing and pausing video, and handling volume and scrubber seekbar

--- app/src/main/java/com/example/android_practice12_audio/MainActivity.java
+++ app/src/main/java/com/example/android_practice12_audio/MainActivity.java
@@ -2,13 +2,92 @@ package com.example.android_practice12_audio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 1st step - declare MediaPlayer and AudioManager
+    MediaPlayer mediaPlayer;
+    AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar volumeControl = findViewById(R.id.seekBar);
+        SeekBar scrubSeekBar =  findViewById(R.id.seekBar2);
+
+        // 2nd step - instantiate the mediaPlayer and audioManager
+        mediaPlayer = MediaPlayer.create(this, R.raw.swish);
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        // 3rd step - for volume: set the values for volume seekbar and provide change listener for volume seekbar
+        volumeControl.setMax(maxVolume);
+        volumeControl.setProgress(currentVolume);
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,progress,0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        // 4th step - for scrubber: set the values for scrubber seekbar and provide change listener for scrubber seekbar
+        scrubSeekBar.setMax(mediaPlayer.getDuration());
+
+        scrubSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mediaPlayer.seekTo(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        // 5th step - provide timer that wil execute particular task written inside run method after specific time
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                scrubSeekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        },0,300);
+    }
+
+    public void play(View v) {
+        mediaPlayer.start();
+    }
+
+    public void pause(View v) {
+        mediaPlayer.pause();
     }
 }
